7f77c3a93e5d15e3a38d6cb69926c77d0a7bc2c0
sweethomeenxovais25 <comercial.sweethomecomfort@gmail.com>
2026-02-22 23:04:56 -0300

    Update app_teste.py

--- app_teste.py
+++ app_teste.py
@@ -103,7 +103,7 @@ def upload_para_cloudinary(file_bytes, file_name, pasta_destino):
             file_bytes,
             folder=caminho_pasta,
             public_id=file_name,
-            resource_type="auto" # Mágica: Aceita PDF e Imagem automaticamente
+            resource_type="auto"
         )
         # Retorna o ID único e o link direto
         return resposta.get('public_id'), resposta.get('secure_url')
