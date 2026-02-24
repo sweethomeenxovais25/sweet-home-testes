a4390de4f8b726cf6e21b675eaa24eb919eaaec9
sweethomeenxovais25 <comercial.sweethomecomfort@gmail.com>
2026-02-24 16:55:38 -0300

    Update app_teste.py

--- app_teste.py
+++ app_teste.py
@@ -988,7 +988,6 @@ elif menu_selecionado == "💰 Financeiro":
             else:
                 st.info("🕒 O histórico aparecerá após o primeiro recebimento ser registrado.")
 
-    st.divider()
     # ====================================================
     # ⚖️ PAINEL GERENCIAL DE INADIMPLÊNCIA E ACORDOS
     # ====================================================
